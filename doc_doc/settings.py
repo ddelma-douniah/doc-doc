8dcb5570f40c2e31851193000a61e43ebbb8abab
ddelma <daniel.delma@douniah.com>
2025-12-09 21:19:04 +0100

    new file:   .dockerignore new file:   Dockerfile modified:   doc_doc/settings.py new file:   docker-compose.yml new file:   requirements.txt

--- doc_doc/settings.py
+++ doc_doc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-4ljx5pbt9c98%4nww*0c=irj1g#2wz$%e+pc(g-&=_)t9slwn!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['doc-doc.douniah.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
